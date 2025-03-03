3cbcaa775eb8b9a7eaa204129c9fff5eb65c6ec4
Jack <jake44264@hotmail.co.uk>
2025-03-03 23:23:40 +0000

    feat: general backend adjustments

--- cmd/initializers/websocket.go
+++ cmd/initializers/websocket.go
@@ -1,6 +1,7 @@
 package initializers
 
 import (
+	"errors"
 	"slices"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 var SocketIO *socketio.Io
 
 var ConnectedUsers []*structs.User
+var AvailableRooms []string = []string{"room1", "room2", "room3"}
 
 func InitializeWebsocket() {
 	SocketIO = socketio.New()
@@ -19,12 +21,21 @@ func InitializeWebsocket() {
 	SocketIO.OnConnection(onConnection)
 }
 
+func getConnectedUser(socketId string) (*structs.User, error) {
+	for _, user := range ConnectedUsers {
+		if user.SocketId == socketId {
+			return user, nil
+		}
+	}
+
+	return nil, errors.New("user not found")
+}
+
 func onConnection(socket *socketio.Socket) {
 	log.Info("New connection from: ", socket.Id)
-	socket.Emit("message", "Hello, world!")
 
 	// We need the user's username. The usernames must be unique (no other user connected can have the same username)
-	socket.On("connect", func(event *socketio.EventPayload) {
+	socket.On("authenticate", func(event *socketio.EventPayload) {
 		if len(event.Data) > 0 && event.Data[0] != nil {
 			username := event.Data[0].(string)
 
@@ -44,10 +55,18 @@ func onConnection(socket *socketio.Socket) {
 			}
 
 			ConnectedUsers = append(ConnectedUsers, user)
+
+			authenticatedResponse := &structs.AuthenticatedResponse{
+				User:  user,
+				Rooms: AvailableRooms,
+			}
+
+			// return success - the user is now authenticated - user information returned
+			socket.Emit("authenticated", authenticatedResponse)
 		}
 	})
 
-	socket.On("join-room", func(event *socketio.EventPayload) {
+	socket.On("joinRoom", func(event *socketio.EventPayload) {
 
 		if len(socket.Rooms()) > 0 {
 			socket.Emit("error", "You cannot be in more than 1 rooms at a time")
@@ -57,39 +76,74 @@ func onConnection(socket *socketio.Socket) {
 		if len(event.Data) > 0 && event.Data[0] != nil {
 			room := event.Data[0].(string)
 			log.Info("Join room: ", room)
-			socket.Join(room)
-		}
-	})
 
-	socket.On("leave-room", func(event *socketio.EventPayload) {
-		if len(event.Data) > 0 && event.Data[0] != nil {
-			room := event.Data[0].(string)
-
-			// Check if the user is in the room
-			rooms := socket.Rooms()
-			if slices.Contains(rooms, room) {
-				socket.Emit("error", "You are not in the room")
+			// check if the room exists
+			if !slices.Contains(AvailableRooms, room) {
+				socket.Emit("error", "Room does not exist")
 				return
 			}
 
-			log.Info("Leave room: ", room)
-			socket.Leave(room)
+			socket.Join(room)
+			socket.Emit("joinedRoom", room)
+			log.Info("User joined room: ", room)
 		}
 	})
 
-	socket.On("message", func(event *socketio.EventPayload) {
+	socket.On("leaveRoom", func(event *socketio.EventPayload) {
+		// no body needed
+
+		if len(socket.Rooms()) == 0 {
+			socket.Emit("error", "You are not in any rooms")
+			return
+		}
+
+		// Make sure the user is in the room
+		rooms := socket.Rooms()
+		foundRoom := ""
+
+		for _, room := range rooms {
+			if slices.Contains(AvailableRooms, room) {
+				foundRoom = room
+				break
+			}
+		}
+
+		if foundRoom == "" {
+			socket.Emit("error", "You are not in the room")
+			return
+		}
+
+
+		log.Info("Leave room: ", foundRoom)
+		socket.Leave(foundRoom)
+		socket.Emit("leftRoom", foundRoom)
+	})
+
+	socket.On("sendMessage", func(event *socketio.EventPayload) {
+
+		// Check if the user is authenticated
+		user, err := getConnectedUser(socket.Id)
+		if err != nil {
+			socket.Emit("error", "User is not authenticated")
+			return
+		}
+
 		if len(event.Data) > 0 && event.Data[0] != nil {
 			message := event.Data[0].(string)
 			log.Info("Message: ", message)
+
+			messageResponse := &structs.MessageResponse{
+				User:        user,
+				Message:     message,
+				ProcessedAt: time.Now(),
+			}
 
 			// get the room the user is in
 			rooms := socket.Rooms()
 			for _, room := range rooms {
 				// broadcast the message to all users in the room
-				socket.To(room).Emit("message", message)
+				socket.To(room).Emit("message", messageResponse)
 			}
-
-			socket.Emit("message", message)
 		}
 	})
 
--- pkg/structs/socket.go
+++ pkg/structs/socket.go
@@ -5,7 +5,18 @@ import (
 )
 
 type User struct {
-	SocketId    string       `json:"socket_id"`
+	SocketId    string    `json:"socket_id"`
 	Username    string    `json:"username"`
 	ConnectedAt time.Time `json:"connected_at"`
+}
+
+type AuthenticatedResponse struct {
+	User  *User    `json:"user"`
+	Rooms []string `json:"rooms"`
+}
+
+type MessageResponse struct {
+	User        *User     `json:"user"`
+	Message     string    `json:"message"`
+	ProcessedAt time.Time `json:"processed_at"`
 }
